b8a5ed75cde3a3ead3e0617c77028a34a28016ba
GDaria-ent <red_potter@mail.ru>
2021-03-19 14:28:59 +0300

    added js

---
+++ scripts.js
@@ -0,0 +1,28 @@
+var modalCloseButton = document.querySelector('.close-modal');
+var textFieldButton = document.querySelector('.search-button');
+var modalWindow = document.querySelector('.modal-wrapper');
+
+textFieldButton.addEventListener('click', function (event){
+    event.preventDefault();
+    modalWindow.classList.add('modal-wrapper--visible');
+});
+
+modalCloseButton.addEventListener('click', ()=>{
+    modalWindow.classList.remove('modal-wrapper--visible');
+});
+
+var whatToDoList = ['Забудь о проблемах', 'Сними стресс', 'Покушай котлетьку', 'Двигайся вперед', 'Покушай курочку', 'Отдохни', 'Позвони маме', 'Попей чайку', 'Делай свое дело', 'Иди к поставленной цели', 'Используй новые возможности', 'Оставь все страхи позади', 'Принимай решения', 'Хватай удачу за хвост', 'Займись своим здоровьем', 'Побалуй себя'];
+var howToDoList = ['быстро,', 'аккуратно,', 'гордо,', 'осторожненько,', 'хитро,', 'божественно,', 'с умом,', 'забыв обо всем,', 'быстро, ловко и умело,', 'по-королевски,', 'играючи,', 'зыркая по сторонам,', 'сосредоточенно,', 'шустренько,', 'со вкусом,', 'ироничненько,'];
+var likeWhatToDoList = ['как парящий орел', 'как смелый скунс', 'как немытый карп', 'как лазурный пес', 'как гордый лев', 'как храбрый кабанчик', 'как прыткая лиса', 'как начальник', 'как мудрец', 'как ласковый котя', 'как веселый щенок', 'как агрессивный гусь', 'как теплый хлебушек', 'как румяный пирожочек', 'как главный бухгалтер', 'как жесточайший батя'];
+
+function randomPhrase(list1, list2, list3){
+    randomIndex1 = Math.floor(Math.random() * list1.length);
+    randomIndex2 = Math.floor(Math.random() * list1.length);
+    randomIndex3 = Math.floor(Math.random() * list1.length);
+    return list1[randomIndex1] + ' ' + list2[randomIndex2] + ' ' + list3[randomIndex3] + '!';
+}
+
+theAnswer = randomPhrase(whatToDoList, howToDoList, likeWhatToDoList);
+console.log(theAnswer);
+
+
